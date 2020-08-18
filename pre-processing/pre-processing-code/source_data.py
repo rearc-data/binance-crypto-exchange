13ee11cb3b44c0b7b1ed48629f2e15f6b63a41fa
Ayush <ayush.varma@rearc.io>
2020-08-18 11:26:45 -0500

    Add ScheduleExpression and fix for aws

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -22,30 +22,28 @@ def data_to_s3(frmt):
         raise Exception('URLError: ', e.reason, frmt)
 
     else:
-      #  data_set_name = os.environ['DATA_SET_NAME']
-        data_set_name = 'AVtest'
+        data_set_name = os.environ['DATA_SET_NAME']
         filename = data_set_name + frmt
         file_location = '/tmp/' + filename
 
         with open(file_location, 'wb') as f:
-            print('hi')
             f.write(response.read())
             f.close()
 
         # variables/resources used to upload to s3
-        # s3_bucket = os.environ['S3_BUCKET']
-        # new_s3_key = data_set_name + '/dataset/'
-        # s3 = boto3.client('s3')
+        s3_bucket = os.environ['S3_BUCKET']
+        new_s3_key = data_set_name + '/dataset/'
+        s3 = boto3.client('s3')
 
-        # s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
+        s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
 
         print('Uploaded: ' + filename)
 
         # deletes to preserve limited space in aws lamdba
-        # os.remove(file_location)
+        os.remove(file_location)
 
         # dicts to be used to add assets to the dataset revision
-        # return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
+        return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
 
 
 def source_dataset():
@@ -119,5 +117,3 @@ def source_dataset():
     # asset_list is returned to be used in lamdba_handler function
     return asset_list
 
-
-source_dataset()

2ae846fdc7a93aefd57dadcf99b437d221e371f5
Ayush <ayush.varma@rearc.io>
2020-12-24 02:58:37 -0600

    Add hashcheck

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,54 +1,60 @@
 import os
 import boto3
+import time
+import ssl
+from multiprocessing.dummy import Pool
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
-from multiprocessing.dummy import Pool
+from s3_md5_compare import md5_compare
 
 
-def data_to_s3(frmt):
-    # throws error occured if there was a problem accessing data
-    # otherwise downloads and uploads to s3
-
+def data_to_s3(endpoint):
     source_dataset_url = 'https://www.cryptodatadownload.com/cdd/Binance_'
-    
+    if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)):
+        ssl._create_default_https_context = ssl._create_unverified_context
+    response = None
+    retries = 5
+    for attempt in range(retries):
+        try:
+            response = urlopen(source_dataset_url + endpoint)
+        except HTTPError as e:
+            if attempt == retries:
+                raise Exception('HTTPError: ', e.code)
+            time.sleep(0.2 * attempt)
+        except URLError as e:
+            if attempt == retries:
+                raise Exception('URLError: ', e.reason)
+            time.sleep(0.2 * attempt)
+        else:
+            break
 
-    try:
-        response = urlopen(source_dataset_url + frmt)
+    if response == None:
+        raise Exception('There was an issue downloading the dataset')
 
-    except HTTPError as e:
-        raise Exception('HTTPError: ', e.code, frmt)
+    data_set_name = os.environ['DATA_SET_NAME']
+    filename = data_set_name + endpoint
+    file_location = '/tmp/' + filename
 
-    except URLError as e:
-        raise Exception('URLError: ', e.reason, frmt)
+    with open(file_location, 'wb') as f:
+        f.write(response.read())
 
-    else:
-        data_set_name = os.environ['DATA_SET_NAME']
-        filename = data_set_name + frmt
-        file_location = '/tmp/' + filename
+    s3_bucket = os.environ['S3_BUCKET']
+    new_s3_key = data_set_name + '/dataset/' + filename
+    s3 = boto3.client('s3')
 
-        with open(file_location, 'wb') as f:
-            f.write(response.read())
-            f.close()
-
-        # variables/resources used to upload to s3
-        s3_bucket = os.environ['S3_BUCKET']
-        new_s3_key = data_set_name + '/dataset/'
-        s3 = boto3.client('s3')
-
-        s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
-
+    has_changes = md5_compare(s3, s3_bucket, new_s3_key, file_location)
+    if has_changes:
+        s3.upload_file(file_location, s3_bucket, new_s3_key)
         print('Uploaded: ' + filename)
+    else:
+        print('No changes in: ' + filename)
 
-        # deletes to preserve limited space in aws lamdba
-        os.remove(file_location)
-
-        # dicts to be used to add assets to the dataset revision
-        return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
+    asset_source = {'Bucket': s3_bucket, 'Key': new_s3_key}
+    return {'has_changes': has_changes, 'asset_source': asset_source}
 
 
 def source_dataset():
-
-    # list of enpoints to be used to access data included with product
     data_endpoints = [
         'BTCUSDT_d.csv',
         'ETHUSDT_d.csv',
@@ -110,10 +116,18 @@ def source_dataset():
 
     ]
 
-    # multithreading speed up accessing data, making lambda run quicker
     with (Pool(2)) as p:
-        asset_list = p.map(data_to_s3, data_endpoints)
+        s3_uploads = p.map(data_to_s3, data_endpoints)
 
-    # asset_list is returned to be used in lamdba_handler function
-    return asset_list
+    count_updated_data = sum(
+        upload['has_changes'] == True for upload in s3_uploads)
 
+    if count_updated_data > 0:
+        asset_list = list(
+            map(lambda upload: upload['asset_source'], s3_uploads))
+        if len(asset_list) == 0:
+            raise Exception('Something went wrong when uploading files to s3')
+        return asset_list
+
+    else:
+        return []
